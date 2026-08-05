dda9c5cf265731043d7c6259b58d6a38031ee74b
Benjamin Tang <benjamintang912@gmail.com>
2026-08-04 17:14:25 -0700

    Fetched real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,29 +1,48 @@
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume}) => {
+    const {fs} = usePuterStore();
+    const [resumeUrl , setResumeUrl ] = useState('');
+
+    useEffect(() => {
+      const loadResume = async () => {
+        const blob = await fs.read(imagePath);
+        if(!blob) return;
+        let url = URL.createObjectURL(blob);
+        setResumeUrl(url);
+      }
+
+      loadResume();
+    }, [imagePath]);
+
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex min-w-0 flex-1 flex-col gap-2 text-left">
-                    <h2 className="!text-black font-bold break-words">
+                    {companyName && <h2 className="!text-black font-bold break-words">
                         {companyName}
-                    </h2>
-                    <h3 className="text-lg break-words text-gray-500">
+                    </h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">
                         {jobTitle}
-                    </h3>
+                    </h3>}
+
+                    {!companyName && !jobTitle && <h2 className="text-black font-bold">Resume</h2>}
                 </div>
                 <div className="ml-3 flex-shrink-0">
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-            <div className="resume-card-image-wrapper gradient-border animate-in fade-in duration-1000">
+            {resumeUrl && (<div className="resume-card-image-wrapper gradient-border animate-in fade-in duration-1000">
                 <img
-                    src={imagePath}
+                    src={resumeUrl}
                     alt="resume"
                     className="resume-card-image"
                 />
             </div>
+            )}
         </Link>
     )
 }
